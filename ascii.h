9cfcfea97c530e4144465430f93333f42e7471af
Stephen Holdaway <stecman@gmail.com>
2019-02-05 08:34:41 +1300

    Add missing licence header in ascii.h

--- ascii.h
+++ ascii.h
@@ -1,3 +1,29 @@
+/*
+ *  Project     Segmented LED Display - ASCII Library
+ *  @author     David Madison
+ *  @link       github.com/dmadison/Segmented-LED-Display-ASCII
+ *  @license    MIT - Copyright (c) 2017 David Madison
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
 const uint8_t SevenSegmentASCII[96] = {
 	0b00000000, /* (space) */
 	0b10000110, /* ! */
